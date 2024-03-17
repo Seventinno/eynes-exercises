a784213e9201b35ddea870440aa899c455041dba
Seventinno <145290256+Seventinno@users.noreply.github.com>
2024-03-17 00:13:33 -0300

    Update simple.py

--- simple/simple.py
+++ simple/simple.py
@@ -2,8 +2,22 @@ import random
 
 
 def simple_list():
-    pass
+    # Lista a donde se almacenaran los diccionarios
+    lista = []
+    diccionario = []
+
+    for i in range(1, 11):
+        # numero aleatorio para la edad
+        edad = random.randint(1, 100)
+        diccionario = {"id": i, "age": edad}
+        lista.append(diccionario)
+
+    return lista
 
 
-def sort_list():
-    pass
+def sort_list(lista):
+    lista_arreglada = sorted(lista, key=lambda x: x['age'])
+    return lista_arreglada
+
+
+print(sort_list(simple_list()))
