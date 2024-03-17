e5d297ba251becb19e8472b7b3b69b210fb3f4ac
Seventinno <145290256+Seventinno@users.noreply.github.com>
2024-03-16 23:28:36 -0300

    Update transpose.py

--- matrix/transpose.py
+++ matrix/transpose.py
@@ -1,2 +1,10 @@
 def transpose(matrix):
-    pass
+    fila = len(matrix)
+    columnas = len(matrix[0])
+
+    transpuesta = [[0] * fila for i in range(columnas)]
+    
+    for i in range(fila):
+        for j in range(columnas):
+            transpuesta[j][i] = matrix[i][j]
+    return transpuesta
